b56eb2f7608854929c72f3623ab772d404115864
David Adler <dalberto.adler@gmail.com>
2017-08-01 17:27:26 +0200

    adding login and stuff

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,8 @@ function init(options) {
 	var thisMonth = options.thisMonth
 	var thisMonthName = options.thisMonthName
 	var thisYear = options.thisYear
+	var uid = options.uid
+
 	thisMonthName = thisMonthName.toLowerCase()
 	thisMonthNamePascal = thisMonthName[0].toUpperCase() + thisMonthName.slice(1)
 
@@ -18,13 +20,15 @@ function init(options) {
 	thisDate.setYear(thisYear)
 
 	var elements = [];
+	var values = [];
 	for (var i = 0; i < daysInMonth(thisDate); i ++) {
 		var day = i + 1;
 		var dayString = day < 10 ? '0' + day : day.toString();
 		var key = 'log/' + dayString + '-' + thisMonthName + '-' + thisYear
+		var dbKey = uid + '/' + key
+		console.info('🗝', key);
 		var ref = database.ref(key)
 		var element = $('div[id="' + key + '"]');
-		var values = [];
 		ref.on('value', (function (element) {return function (snap) {
 			var data = snap.val() || {};
 			values.push(data)
@@ -41,17 +45,30 @@ function init(options) {
 			console.info('send');
 			var target = $(event.target);
 			var key = target.parent().attr('data-key')
-			database.ref(key).update({'done': target.is(':checked')})
+			database.ref(uid + '/' + key).update({'done': target.is(':checked')})
 		})
 		element.find('.comment').on('change', function (event) {
 			var target = $(event.target);
 			var key = target.parent().attr('data-key')
-			database.ref(key).update({'comment': target.val()})
+			database.ref(uid + '/' + key).update({'comment': target.val()})
 		})
 		elements.push(element)
 	}
+	setTimeout(function () {
+		console.info('data', values);
+	}, 3000);
 
 	// Set title
 	$('title').text(thisMonthNamePascal + ' ' + thisYear + ' - Everyday I Will For One Month')
+}
 
+
+function facebookLogin() {
+	FB.getLoginStatus(function(response) {
+	    onFacebookLoginStatus(response);
+	});
+}
+
+function onFacebookLoginStatus(response) {
+	console.info(response);
 }
